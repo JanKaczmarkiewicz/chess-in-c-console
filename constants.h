623f09e16f659094a29be655657da716c4da3257
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2020-11-07 08:55:13 +0100

    fix: fix tile sizes

--- constants.h
+++ constants.h
@@ -5,8 +5,8 @@
 #define MAX_MOVES_NUMBER 7*4
 #define NUMBER_OF_SUBROWS 3
 
-#define PRIMARY_SUBROW "\033[3;40;15m   %s   \033[0m"
-#define SECONDARY_SUBROW "\033[3;47;10m   %s   \033[0m"
+#define PRIMARY_SUBROW "\033[38;40;6m   %s   \033[0m"
+#define SECONDARY_SUBROW "\033[46;47;6m   %s   \033[0m"
 #define SELECTED_SUBROW "\033[3;46;35m   %s   \033[0m"
 #define ALLOWED_SUBROW "\033[3;42;35m   %s   \033[0m"
 #define CAPTURE_SUBROW "\033[3;41;35m   %s   \033[0m"
